04dd1bbdfcb3443259a4dec55a8739be14134b93
boussarsar haytham <boussarsarhaytham@gmail.com>
2021-07-01 00:13:21 +0100

    setup websocket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WsGateway } from './ws.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [WsGateway],
 })
 export class AppModule {}
