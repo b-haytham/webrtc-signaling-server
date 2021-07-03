a5e60e1a4154c96456690e3fccf17e50d90b550a
boussarsar haytham <boussarsarhaytham@gmail.com>
2021-07-03 16:25:46 +0100

    assign userfriendly id, handle socket disconnect

--- src/ws.gateway.ts
+++ src/ws.gateway.ts
@@ -1,30 +1,42 @@
 import { ValidationPipe } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, WebSocketServer, MessageBody } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, WebSocketServer, MessageBody, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io'
 import { AnswerCallDto } from './dto/AnswerCallDto';
 import { CallUserEventDto } from './dto/CallUserEvent';
 import { RejectCallEventDto } from './dto/RejectCallDto';
 
 @WebSocketGateway()
-export class WsGateway implements OnGatewayConnection {
+export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
  
   @WebSocketServer()
   server: Server
 
+  private users = {}
+
   async handleConnection(socket: Socket) {
-    socket.emit('me', socket.id)
+    const id = Math.floor(Math.random() * 10000) 
+    this.users[id] = socket.id
+    socket.emit('me', id)
     this.server.to(socket.id).emit('hey', 'Helloo')
+    console.log(this.users)
   }
+
+  async handleDisconnect(socket: Socket) {
+    for(var f in this.users) {
+      if(this.users.hasOwnProperty(f) && this.users[f] == socket.id) {
+          delete this.users[f];
+      }
+  }}
   
   @SubscribeMessage('call.user')
   callUser(@MessageBody(ValidationPipe) data: CallUserEventDto) {
-    this.server.to(data.user_to_call).emit('user.calling', {signal: data.signal, from: data.from})
+    this.server.to(this.users[data.user_to_call]).emit('user.calling', {signal: data.signal, from: data.from})
   }
 
 
   @SubscribeMessage('answer.call')
   answerCall(@MessageBody(ValidationPipe) data: AnswerCallDto) {
-    this.server.to(data.to).emit('call.accepted', {signal: data.signal})
+    this.server.to(this.users[data.to]).emit('call.accepted', {signal: data.signal})
   }
 
   @SubscribeMessage('reject.call')
