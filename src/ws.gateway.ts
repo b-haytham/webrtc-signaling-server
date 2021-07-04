1733ff076bfe570dcecd8e67a8eef63bf26d8cb4
boussarsar haytham <boussarsarhaytham@gmail.com>
2021-07-04 03:36:56 +0100

    handle call cancelled, call ended events

--- src/ws.gateway.ts
+++ src/ws.gateway.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, WebSocketServer, MessageBody, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io'
 import { AnswerCallDto } from './dto/AnswerCallDto';
@@ -7,7 +7,9 @@ import { RejectCallEventDto } from './dto/RejectCallDto';
 
 @WebSocketGateway()
 export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
- 
+  
+  private readonly logger = new Logger(WsGateway.name);
+
   @WebSocketServer()
   server: Server
 
@@ -18,10 +20,11 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.users[id] = socket.id
     socket.emit('me', id)
     this.server.to(socket.id).emit('hey', 'Helloo')
-    console.log(this.users)
+    this.logger.log(`New Socket >> ${socket.id}`)
   }
 
   async handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket Disconnect >> ${socket.id}`)
     for(var f in this.users) {
       if(this.users.hasOwnProperty(f) && this.users[f] == socket.id) {
           delete this.users[f];
@@ -30,22 +33,37 @@ export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
   @SubscribeMessage('call.user')
   callUser(@MessageBody(ValidationPipe) data: CallUserEventDto) {
+    this.logger.log(`Call User Event `)
+    this.logger.log(`Emitting User Calling Event`)
     this.server.to(this.users[data.user_to_call]).emit('user.calling', {signal: data.signal, from: data.from})
   }
 
 
   @SubscribeMessage('answer.call')
   answerCall(@MessageBody(ValidationPipe) data: AnswerCallDto) {
+    this.logger.log(`Answer Call Event `)
+    this.logger.log(`Emitting Call Accepted Event`)
     this.server.to(this.users[data.to]).emit('call.accepted', {signal: data.signal})
   }
 
   @SubscribeMessage('reject.call')
   rejectCall(@MessageBody(ValidationPipe) data: RejectCallEventDto) {
-    this.server.to(data.to).emit('call.rejected', {from: data.from})
+    this.logger.log(`Reject Call Event Event `)
+    this.logger.log(`Emitting Call Rejected Event`)
+    this.server.to(this.users[data.to]).emit('call.rejected', {from: data.from})
+  }
+
+  @SubscribeMessage('cancel.call')
+  cancelCall(@MessageBody(ValidationPipe) data: RejectCallEventDto) {
+    this.logger.log(`Cancel Call Event  `)
+    this.logger.log(`Emitting Call Cancelled Event`)
+    this.server.to(this.users[data.to]).emit('call.cancelled', {from: data.from})
   }
 
   @SubscribeMessage('end.call')
   endCall(@MessageBody(ValidationPipe) data: RejectCallEventDto) {
-    this.server.to(data.to).emit('call.rejected', {from: data.from})
+    this.logger.log(`End Call Event `)
+    this.logger.log(`Emitting Call Ended Event`)
+    this.server.to(this.users[data.to]).emit('call.ended', {from: data.from})
   }
 }
